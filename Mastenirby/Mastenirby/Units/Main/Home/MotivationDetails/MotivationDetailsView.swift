5110934e15e625099fba1d3566a72958a485e2dd
Andrii Momot <andrii.momot.ua@gmail.com>
2024-05-20 12:05:41 +0300

    fixed a bug with Motivation

--- Mastenirby/Mastenirby/Units/Main/Home/MotivationDetails/MotivationDetailsView.swift
+++ Mastenirby/Mastenirby/Units/Main/Home/MotivationDetails/MotivationDetailsView.swift
@@ -26,10 +26,22 @@ struct MotivationDetailsView: View {
                             .frame(height: UIScreen.main.bounds.height * 0.2)
                             .clipped()
                         
-                        Text(item.text)
-                            .foregroundStyle(.black)
-                            .font(Fonts.SFProDisplay.medium.swiftUIFont(size: 15))
-                            .padding()
+                        HStack {
+                            Text(item.title)
+                                .foregroundStyle(.black)
+                            .font(Fonts.SFProDisplay.bold.swiftUIFont(size: 20))
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                        
+                        HStack {
+                            Text(item.text)
+                                .foregroundStyle(.black)
+                                .font(Fonts.SFProDisplay.medium.swiftUIFont(size: 15))
+                            Spacer()
+                        }
+                        .padding(.horizontal)
                     }
                 }
                 
@@ -45,6 +57,7 @@ struct MotivationDetailsView: View {
 }
 
 #Preview {
-    MotivationDetailsView(item: .init(image: Asset.motivation1.name,
-                                      text: "O krok od ekranu  \nAplikacja detoksu cyfrowego jest niezastąpionym towarzyszem w podróży ku nowym przeżyciom. Kiedy zbliżasz się do ekranu, zatrzymaj się na chwilę i skorzystaj z aplikacji, aby odkryć sposoby na delektowanie się chwilą bez nieustannego pochłaniania uwagi przez urządzenia elektroniczne. Dzięki niej możesz zachować swoje zasoby na tę niesamowitą przygodę i skoncentrować się na otaczającej rzeczywistości.\nOtwórz swoje umysłowe horyzonty, wykorzystując aplikację detoksu cyfrowego. To nie tylko narzędzie, ale także inspiracja do odkrywania nowych pomysłów i możliwości, pozostawiając w tyle szum cyfrowego świata. Stwórz własną wersję bezekranowej przygody, zainspirowaną przez aplikację, która pomoże Ci cieszyć się chwilą bez technologicznego rozpraszacza.\nZachęcamy do eksperymentowania z różnymi sposobami korzystania z aplikacji detoksu cyfrowego, aby lepiej zrozumieć, jakie doświadczenia sprawiają Ci największą radość i satysfakcję. Pamiętaj, że podróż bez ekranu może być nie tylko relaksem, ale też szansą na autentyczne odkrywanie siebie i otaczającego świata. Niech aplikacja stanie się Twoim przewodnikiem w tej inspirującej podróży, pozwalającą Ci korzystać z chwil bez ekranu w pełni i z pasją!\nOtwórz nowe horyzonty"))
+    MotivationDetailsView(item: .init(image: Asset.motivation4.name,
+                                      title: "O krok od ekranu",
+                                      text: "Aplikacja detoksu cyfrowego jest niezastąpionym towarzyszem w podróży ku nowym przeżyciom. Kiedy zbliżasz się do ekranu, zatrzymaj się na chwilę i skorzystaj z aplikacji, aby odkryć sposoby na delektowanie się chwilą bez nieustannego pochłaniania uwagi przez urządzenia elektroniczne. Dzięki niej możesz zachować swoje zasoby na tę niesamowitą przygodę i skoncentrować się na otaczającej rzeczywistości.\nOtwórz swoje umysłowe horyzonty, wykorzystując aplikację detoksu cyfrowego. To nie tylko narzędzie, ale także inspiracja do odkrywania nowych pomysłów i możliwości, pozostawiając w tyle szum cyfrowego świata. Stwórz własną wersję bezekranowej przygody, zainspirowaną przez aplikację, która pomoże Ci cieszyć się chwilą bez technologicznego rozpraszacza.\nZachęcamy do eksperymentowania z różnymi sposobami korzystania z aplikacji detoksu cyfrowego, aby lepiej zrozumieć, jakie doświadczenia sprawiają Ci największą radość i satysfakcję. Pamiętaj, że podróż bez ekranu może być nie tylko relaksem, ale też szansą na autentyczne odkrywanie siebie i otaczającego świata. Niech aplikacja stanie się Twoim przewodnikiem w tej inspirującej podróży, pozwalającą Ci korzystać z chwil bez ekranu w pełni i z pasją!"))
 }
--- Mastenirby/Mastenirby/Units/Main/Porada/AdviceViewModel.swift
+++ Mastenirby/Mastenirby/Units/Main/Porada/AdviceViewModel.swift
@@ -10,11 +10,14 @@ import Foundation
 extension AdviceView {
     final class AdviceViewModel: ObservableObject {
         let adviсeItems: [AdviceModel] = [
-            .init(title: "Jakie aktywności mogę wykonywać bez użycia gadżetów?",
-                  description: "Odkryj świat aktywności niewymagających użycia urządzeń cyfrowych za pomocą naszej aplikacji podczas oszczędzania na swoją podróż lub wydarzenie specjalne. Bez względu na to, czy chodzi o eksplorację natury, próbowanie nowych hobby, czy uczestnictwo w doświadczeniach kulturalnych, nasza aplikacja oferuje porady, abyś mógł się bawić bez korzystania z ekranów."),
+            .init(title: "Jak mogę pozostać odłączony podczas mojej podróży?",
+                  description: "Nasza aplikacja budżetowania detoksu cyfrowego może pomóc Ci pozostać zaangażowanym w aktywności bez ekranu podczas podróży lub wydarzeń. Oferuje wskazówki i sugestie dotyczące przyjemnych sposobów na odwrócenie uwagi od ekranów i odkrywania nowych zainteresowań podczas gromadzenia zasobów na swoje przygody."),
             
                 .init(title: "Jakie aktywności mogę wykonywać bez użycia gadżetów?",
-                      description: "Odkryj świat aktywności niewymagających użycia urządzeń cyfrowych za pomocą naszej aplikacji podczas oszczędzania na swoją podróż lub wydarzenie specjalne. Bez względu na to, czy chodzi o eksplorację natury, próbowanie nowych hobby, czy uczestnictwo w doświadczeniach kulturalnych, nasza aplikacja oferuje porady, abyś mógł się bawić bez korzystania z ekranów.")
+                      description: "Odkryj świat aktywności niewymagających użycia urządzeń cyfrowych za pomocą naszej aplikacji podczas oszczędzania na swoją podróż lub wydarzenie specjalne. Bez względu na to, czy chodzi o eksplorację natury, próbowanie nowych hobby, czy uczestnictwo w doświadczeniach kulturalnych, nasza aplikacja oferuje porady, abyś mógł się bawić bez korzystania z ekranów."),
+            
+                .init(title: "Jak najlepiej wykorzystać swoje chwile bez ekranu?",
+                      description: "Podczas budżetowania na swoją podróż, nasza aplikacja detoksu cyfrowego oferuje cenne wskazówki, jak najlepiej wykorzystać czas bez ekranu. Naucz się cieszyć offlineowymi doświadczeniami, być w kontakcie ze swoim otoczeniem i cieszyć się znaczącymi interakcjami – wszystko podczas przygotowań do następnej przygody.")
         ]
     }
 }
